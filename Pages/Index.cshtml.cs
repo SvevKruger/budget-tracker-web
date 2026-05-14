e7a3091d8e6a9cdf317f2954b88e83954f1d55ef
Filip <filipobrenovic@hotmail.com>
2026-05-15 00:17:52 +0200

    add transaction table to web app

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace BudgetTrackerWeb.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    public List<string> Descriptions = new List<string>();
+    public List<double> Amounts = new List<double>();
+    public List<string> Dates = new List<string>();
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    private string filepath = "transactions.txt";
 
     public void OnGet()
     {
-
+        if (System.IO.File.Exists(filepath))
+        {
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                Descriptions.Add(parts[0]);
+                Amounts.Add(double.Parse(parts[1]));
+                Dates.Add(parts[2]);
+            }
+        }
     }
 }
